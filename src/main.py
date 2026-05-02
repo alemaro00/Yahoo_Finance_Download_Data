85169c0722ef10520ad1996fd6c48b2b8b188866
alemaro00 <alessio.maro00@gmail.com>
2026-05-02 12:53:15 +0200

    Initial commit

---
+++ src/main.py
@@ -0,0 +1,9 @@
+from modules.num import print_num
+
+def main():
+    n = 10  # You can change this value to print a different size diamond
+    m = 5
+    print_num(n)
+
+if __name__ == "__main__":
+    main()
